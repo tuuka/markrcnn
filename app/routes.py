b124ba58aad0114add347433560a1070a329bad5
tuuka <scientist74@gmail.com>
2020-02-10 17:17:22 +0300

    predict route is added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 from app import application
-import torch, torchvision, os, sys, requests
+from flask import jsonify, request
+import torch, torchvision, os, sys, requests, io
 from urllib.request import urlretrieve
+from PIL import Image
 
 @application.route('/')
 @application.route('/index')
@@ -11,11 +13,70 @@ def index():
     backbone = load_model(None, model_urls['backbone_qnnpack'])
     return "Hello, World!"
 
+@application.route('/predict', methods=['GET', 'POST'])
+def predict():
+
+    if request.method == 'POST':
+        if 'file' not in request.files:
+            return jsonify({'error':'No source img file'})
+        print('request_files: ', request.files)
+        file = request.files.get('file')
+        if not file:
+            return jsonify({'error':'Not correct source img file'})
+
+        image_bytes = file.read()
+        #print('file: ', img_bytes)
+
+        #try:
+        img, orig_size = transform_image(image_bytes,
+                                         mean=[0, 0, 0],
+                                         std=[1, 1, 1])  # Dont normalize coco 2017 cause pytorch models uses normalization themself
+        img = img.squeeze()
+        # print('Memory in detection/utils/get_prediction before predicting and img_memory_size: ', memorycheck(img))
+        torch.backends.quantized.engine = 'qnnpack'
+        model = load_model(None, model_urls['model'])
+
+        print('Model loaded. Object detection predicting...')
+        model.eval()
+
+        print('Processed image shape: ', img.size())
+        #print('Original image size: ', orig_size)
+
+        with torch.no_grad():
+            prediction = model([img])[0]
+
+        #except Exception as e:
+        #    print('Can not predict! Internal error:', e)
+        #    return jsonify({'error': 'Can not predict. Please try another image.'})
+
+        #print(prediction)
+        pred = {'error': ''}
+        pred['prediction'] = prediction
+
+        print('Done!')
+        #print('prediction: ', pred['prediction'])
+        return jsonify(pred)
 
 
 
-    return "Hello, World!"
 
+def transform_image(image_bytes, mean = [0.485, 0.456, 0.406], std = [0.229, 0.224, 0.225], device=torch.device('cpu')):
+    #res = 320 if str(device) == 'cpu' else 640
+    #print('Working on {}. Sizing to {}px'.format(device, res))
+    my_transforms = torchvision.transforms.Compose([
+        #torchvision.transforms.Resize(res),
+        torchvision.transforms.ToTensor(),
+        torchvision.transforms.Normalize(mean,std)]
+    )
+    # print(io.BytesIO(image_bytes))
+    try:
+        image = Image.open(io.BytesIO(image_bytes)).convert('RGB')
+        orig_size = image.size
+        #print(image)
+        return my_transforms(image).unsqueeze(0), orig_size
+    except Exception as e:
+        print('Exception was raised while transforming image: ',e)
+        return ''
 
 
 
@@ -97,148 +158,6 @@ def download_file_from_google_drive(id, destination):
         params = {'id': id, 'confirm': token}
         response = session.get(URL, params=params, stream=True)
     save_response_content(response, destination)
-
-
-from collections import OrderedDict
-
-import torch
-import torch.nn.functional as F
-from torch import nn, Tensor
-
-from torch.jit.annotations import Tuple, List, Dict
-
-
-# Here we quntize/dequantize tensors only in BackboneWithFPN class
-
-class IntermediateLayerGetterQuant(torchvision.models._utils.IntermediateLayerGetter):
-
-    def __init__(self, model=torchvision.models.quantization.resnet50(pretrained=False, quantize=False,
-                                                                      norm_layer=torch.nn.BatchNorm2d),
-                 return_layers={'layer1': '0', 'layer2': '1', 'layer3': '2', 'layer4': '3'}):
-        if not set(return_layers).issubset([name for name, _ in model.named_children()]):
-            raise ValueError("return_layers are not present in model")
-        super(IntermediateLayerGetterQuant, self).__init__(model, return_layers)
-
-    def fuse_model(self):
-        torch.quantization.fuse_modules(self, ['conv1', 'bn1', 'relu'], inplace=True)
-        for m in self.modules():
-            if type(m) == torchvision.models.quantization.resnet.QuantizableBottleneck or \
-                    type(m) == torchvision.models.quantization.resnet.QuantizableBasicBlock:
-                m.fuse_model()
-
-
-class FeaturePyramidNetworkQuant(nn.Module):
-    __constants__ = ['inner_blocks', 'layer_blocks']
-
-    def __init__(self, in_channels_list, out_channels, extra_blocks=None):
-        super(FeaturePyramidNetworkQuant, self).__init__()
-        self.inner_blocks = nn.ModuleList()
-        self.layer_blocks = nn.ModuleList()
-        # self.quant = torch.quantization.QuantStub()
-        # self.dequant = torch.quantization.DeQuantStub()
-        self.FPN_add = nn.quantized.FloatFunctional()
-        for in_channels in in_channels_list:
-            if in_channels == 0:
-                continue
-            inner_block_module = nn.Conv2d(in_channels, out_channels, 1)
-            layer_block_module = nn.Conv2d(out_channels, out_channels, 3, padding=1)
-            self.inner_blocks.append(inner_block_module)
-            self.layer_blocks.append(layer_block_module)
-
-        # initialize parameters now to avoid modifying the initialization of top_blocks
-        for m in self.children():
-            if isinstance(m, nn.Conv2d):
-                nn.init.kaiming_uniform_(m.weight, a=1)
-                nn.init.constant_(m.bias, 0)
-
-        # if extra_blocks is not None:
-        #    assert isinstance(extra_blocks, ExtraFPNBlock)
-        # self.extra_blocks = extra_blocks
-
-    def forward(self, x: Dict[str, torch.Tensor]):
-
-        # unpack OrderedDict into two lists for easier handling
-        names = list(x.keys())
-        x = list(x.values())
-
-        # Assume that input quanted already, otherwise uncomment it
-        # x = [self.quant(i) for i in x]
-
-        # Some changes in original code for torchscript
-        # cause torchscripit can only iterate through the ModuleList:
-        in_results = []
-        i = 0
-        for mod in self.inner_blocks:
-            in_results.append(mod(x[i]))
-            i += 1
-        l_results = [in_results[-1]]
-        i = len(in_results) - 2
-        while i >= 0:
-            feat_shape = in_results[i].shape[-2:]
-            inner_top_down = torch.nn.functional.interpolate(l_results[0], size=feat_shape, mode="nearest")
-            l_results.insert(0, self.FPN_add.add(in_results[i], inner_top_down))
-            i -= 1
-
-        i = 0
-        results = []
-        for mod in self.layer_blocks:
-            results.append(mod(l_results[i]))
-            i += 1
-
-        # make it back an OrderedDict
-        out = OrderedDict([(k, v) for k, v in zip(names, results)])
-        # if self.extra_blocks is not None:
-        out['pool'] = torch.nn.functional.max_pool2d(results[-1], 1, 2, 0)
-
-        return out
-
-
-class BackboneWithFPN(nn.Module):
-
-    def __init__(self, backbone, return_layers, in_channels_list, out_channels):
-        super().__init__()
-        self.body = IntermediateLayerGetterQuant(backbone, return_layers=return_layers)
-        self.fpn = FeaturePyramidNetworkQuant(
-            in_channels_list=in_channels_list,
-            out_channels=out_channels,
-        )
-        self.quant = torch.quantization.QuantStub()
-        self.dequant = torch.quantization.DeQuantStub()
-        self.out_channels = out_channels
-
-    def forward(self, input):
-        out = self.fpn(self.body(self.quant(input)))
-        out = OrderedDict([(k, self.dequant(v)) for k, v in out.items()])
-        return out
-
-    def fuse_model(self):
-        # torch.quantization.fuse_modules(self, ['conv1', 'bn1', 'relu'], inplace=True)
-        for m in self.modules():
-            if type(m) == IntermediateLayerGetterQuant:
-                m.fuse_model()
-
-
-def resnet50_fpn_backbone(backbone=None, pretrained=False):
-    if backbone is None:
-        # backbone = torchvision.models.quantization.resnet50(pretrained=False, quantize=False, norm_layer=torchvision.ops.misc.FrozenBatchNorm2d)
-        backbone = torchvision.models.quantization.resnet50(pretrained=False, quantize=False,
-                                                            norm_layer=torch.nn.BatchNorm2d)
-    # freeze layers
-    for name, parameter in backbone.named_parameters():
-        if 'layer2' not in name and 'layer3' not in name and 'layer4' not in name:
-            parameter.requires_grad_(False)
-
-    return_layers = {'layer1': '0', 'layer2': '1', 'layer3': '2', 'layer4': '3'}
-
-    in_channels_stage2 = backbone.inplanes // 8
-    in_channels_list = [
-        in_channels_stage2,
-        in_channels_stage2 * 2,
-        in_channels_stage2 * 4,
-        in_channels_stage2 * 8,
-    ]
-    out_channels = 256
-    return BackboneWithFPN(backbone, return_layers, in_channels_list, out_channels)
 
 
 
