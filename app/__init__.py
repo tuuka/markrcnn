e22d3217e7c3b9546aec24adc146862112a14f5c
tuuka <scientist74@gmail.com>
2020-03-06 23:50:51 +0300

    Flask-cache was added

--- app/__init__.py
+++ app/__init__.py
@@ -3,11 +3,12 @@ from flask_cors import CORS
 from flask_caching import Cache
 
 
-#cache = Cache(config={'CACHE_TYPE': 'simple'})
+cache = Cache(config={'CACHE_TYPE': 'simple'})
+'''
 cache = Cache(config={
     "CACHE_TYPE": "filesystem",
     "CACHE_DIR": "/tmp/cached"
-})
+})'''
 application = Flask(__name__)
 #cache.init_app(application)
 cache.init_app(application)
